c70398d351a70eb6d244d2bdae096243fe7973a3
Handrys <dumitru.k@tech-titans.md>
2025-08-13 23:23:38 +0300

    change redis link

--- redisClient.js
+++ redisClient.js
@@ -1,7 +1,8 @@
 // redisClient.js
 import redis from 'redis';
 
-const client = redis.createClient({ url: 'redis://localhost:6379' });
+/* const client = redis.createClient({ url: 'redis://localhost:6379' }); */
+const client = redis.createClient({ url: 'redis://red-d2ef6i3ipnbc739ugjd0:6379' });
 
 client.on('error', (err) => console.error('Redis Client Error', err));
 
